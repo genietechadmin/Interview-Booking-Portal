6a8bb519a1c64641e700827b2c98b4a62076830e
jothika-12 <jothika12.2005@gmail.com>
2026-06-17 15:48:02 +0530

    Remove remaining service-account.json dependency

--- backend/services/bookingSheetService.js
+++ backend/services/bookingSheetService.js
@@ -2,8 +2,14 @@ const { google } = require("googleapis");
 const path = require("path");
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, "../credentials/service-account.json"),
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  credentials: {
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  },
+  scopes: [
+    "https://www.googleapis.com/auth/spreadsheets",
+  ],
 });
 
 const BOOKINGS_RANGE = "Bookings!A:O";
